c7ff58c5c0c099f6e851c8af532ea5cfe6ec0506
RbsilvaLeandro <rbsilva.leandro@gmail.com>
2021-10-17 21:46:29 -0300

    Refactoring

---
+++ LCesarAdvogados.Tests/HomeTests.cs
@@ -0,0 +1,24 @@
+﻿using LCesarAdvogados.Aplicacao;
+using LCesarAdvogados.Aplicacao.Interface;
+using LCesarAdvogados.Dominio.Entidades;
+using LCesarAdvogados.Dominio.Interfaces.Repositorios;
+using LCesarAdvogados.Dominio.Interfaces.Servicos;
+using LCesarAdvogados.Infra.Repositorios;
+using LCesarAdvogados.MVC.Controllers;
+using Moq;
+using System.Web.Mvc;
+using Xunit;
+
+namespace LCesarAdvogados.Tests
+{
+    public class HomeTests
+    {
+        [Fact]
+        public void Should_Return_View_Index()
+        {
+                        
+        }
+
+
+    }
+}
